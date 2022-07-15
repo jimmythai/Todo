20f82ba258ef181dd61e0ffad3ad4e698f9fa6b9
AUTHOR_NAME <AUTHOR_EMAIL>
2022-07-15 17:21:03 +0700

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Atsushi Yamamoto on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
